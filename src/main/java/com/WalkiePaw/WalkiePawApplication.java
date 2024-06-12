d1e3fc8a1645f6d2d4c01716e5436f7af13cc021
Chaerin <chaexxrin@gmail.com>
2024-06-12 14:59:42 +0900

    .gitignore 수정

--- src/main/java/com/WalkiePaw/WalkiePawApplication.java
+++ src/main/java/com/WalkiePaw/WalkiePawApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalkiePawApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WalkiePawApplication.class, args);
-
-		int a = 0;
-	}
+	public static void main(String[] args) {SpringApplication.run(WalkiePawApplication.class, args);}
 
 }
