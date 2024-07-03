6f4d8611d465ea7100a8e996907dcea43dc63f02
sunjong0214 <rlatjswhd8200@gmail.com>
2024-07-03 19:26:25 +0900

    Feat: chatmsg list response

--- src/main/java/com/WalkiePaw/presentation/domain/chat/dto/ChatMsgListResponse.java
+++ src/main/java/com/WalkiePaw/presentation/domain/chat/dto/ChatMsgListResponse.java
@@ -4,11 +4,17 @@ import com.WalkiePaw.domain.chat.entity.ChatMessage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class ChatMsgListResponse {
 
+    private final String nickname;
+    private final String content;
+    private final LocalDateTime createDate;
+
     public static ChatMsgListResponse from(ChatMessage chatMessage) {
-        return new ChatMsgListResponse();
+        return new ChatMsgListResponse(chatMessage.getWriter().getNickname(), chatMessage.getContent(), chatMessage.getCreatedDate());
     }
 }
