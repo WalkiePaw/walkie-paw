772a555fbc77fb2e53b7d44ffd3225f645977c19
sunjong0214 <rlatjswhd8200@gmail.com>
2024-07-05 18:25:37 +0900

    Refactor: ì구사항

--- src/main/java/com/WalkiePaw/presentation/domain/chatroom/dto/ChatroomListResponse.java
+++ src/main/java/com/WalkiePaw/presentation/domain/chatroom/dto/ChatroomListResponse.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
@@ -13,11 +14,11 @@ public class ChatroomListResponse {
     private final String location;
     private final String nickname;
     private final String latestMessage;
-    private final LocalDateTime latestTime;
+    private final LocalTime latestTime;
     private final int unreadCount;
 
     public static ChatroomListResponse from(final Chatroom chatroom) {
         return new ChatroomListResponse(chatroom.getId(), chatroom.getBoard().getLocation(),
-                 chatroom.getMember().getNickname(), chatroom.getLatestMessage(), chatroom.getModifiedDate(), chatroom.getUnreadCount());
+                 chatroom.getMember().getNickname(), chatroom.getLatestMessage(), chatroom.getModifiedDate().toLocalTime(), chatroom.getUnreadCount());
     }
 }
