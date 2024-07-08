6a134ae89167b7bdd5eef873f9c56d009a6f0366
sunjong0214 <rlatjswhd8200@gmail.com>
2024-07-08 11:03:45 +0900

    ãRefactor: url 객체 생성

--- src/main/java/com/WalkiePaw/presentation/domain/upload/UploadController.java
+++ src/main/java/com/WalkiePaw/presentation/domain/upload/UploadController.java
@@ -2,13 +2,12 @@ package com.WalkiePaw.presentation.domain.upload;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/uploads")
 public class UploadController {
@@ -16,8 +15,8 @@ public class UploadController {
     private final UploadService uploadService;
 
     @PostMapping
-    public ResponseEntity<String> uploadImage(@RequestParam MultipartFile file) {
+    public ResponseEntity<UploadUrlResponse> uploadImage(@RequestParam MultipartFile file) {
         String uploadURL = uploadService.upload(file);
-        return ResponseEntity.ok(uploadURL);
+        return ResponseEntity.ok(new UploadUrlResponse(uploadURL));
     }
 }
