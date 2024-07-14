deca39817c55b67774fa9999975ea958502f5502
woo-be <thsckddn100@gmail.com>
2024-07-15 01:28:47 +0900

    deploy.yml, Dockerfile, properties

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:21
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]\
+ENTRYPOINT ["java","-jar","-Dspring.profiles.active=prod, spring-data-jpa","/app.jar"]\
---
+++ src/main/java/com/WalkiePaw/utils/YamlPropertySourceFactory.java
@@ -0,0 +1,30 @@
+package com.WalkiePaw.utils;
+
+import io.micrometer.common.lang.Nullable;
+import java.util.Objects;
+import java.util.Properties;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.util.StringUtils;
+
+/**
+ * @PropertySource 에서 .yml파일을 사용하기 위한 클래스
+ */
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+  @Override
+  public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) {
+    Properties yamlProperties = loadYamlProperties(resource);
+    String sourceName = StringUtils.hasText(name) ? name : resource.getResource().getFilename();
+    return new PropertiesPropertySource(Objects.requireNonNull(sourceName), Objects.requireNonNull(yamlProperties));
+  }
+
+  private Properties loadYamlProperties(EncodedResource resource) {
+    YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+    factory.setResources(resource.getResource());
+    return factory.getObject();
+  }
+}
