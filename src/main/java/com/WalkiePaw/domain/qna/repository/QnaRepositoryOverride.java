8a4aede9542ed612d26af6df14026aa94f7c8591
woo-be <thsckddn100@gmail.com>
2024-07-17 11:45:21 +0900

    Feat: 문의 리스트 페이징 처리
    - 관리자 페이지 문의 리스트 페이징 처리
    - 마이페이지 문의 리스트 페이징 처리

--- src/main/java/com/WalkiePaw/domain/qna/repository/QnaRepositoryOverride.java
+++ src/main/java/com/WalkiePaw/domain/qna/repository/QnaRepositoryOverride.java
@@ -1,12 +1,15 @@
 package com.WalkiePaw.domain.qna.repository;
 
 import com.WalkiePaw.domain.qna.entity.Qna;
+import com.WalkiePaw.presentation.domain.qna.dto.QnaListResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface QnaRepositoryOverride {
 
-    List<Qna> findAllByCond(String status);
+    Page<QnaListResponse> findAllByCond(String status, Pageable pageable);
 
-    List<Qna> findByMemberId(Integer memberId);
+    Page<QnaListResponse> findByMemberId(Integer memberId, Pageable pageable);
 }
