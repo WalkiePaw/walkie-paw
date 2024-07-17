f6c37ce8abfe8dfcf59ff37ed23d6c8639e7db05
sunjong0214 <rlatjswhd8200@gmail.com>
2024-07-17 16:05:54 +0900

    Fix: 수정

--- src/main/java/com/WalkiePaw/domain/chatroom/entity/Chatroom.java
+++ src/main/java/com/WalkiePaw/domain/chatroom/entity/Chatroom.java
@@ -41,7 +41,7 @@ public class Chatroom extends BaseEntity {
     public Chatroom(Board board, Member member) {
         this.board = board;
         this.member = member;
-        this.status = ChatroomStatus.GENERAL;
+        this.status = ChatroomStatus.RECRUITING;
     }
 
     public void updateLatestMessage(final String latestMessage) {
